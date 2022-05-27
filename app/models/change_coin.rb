146230461eb176328b42c62fab94f19362519797
katarigod <tomoki-katari@outlook.jp>
2022-05-27 08:48:57 +0000

    修正

--- app/models/change_coin.rb
+++ app/models/change_coin.rb
@@ -1,4 +1,4 @@
 class ChangeCoin < ApplicationRecord
   belongs_to :customer, optional: true
-  validates :piece, presence: true
+  validates :piece, presence: true, numericality: {only_integer: true}, length: { maximum: 5 }
 end
